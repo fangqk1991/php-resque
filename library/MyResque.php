6acc0bdc422057657621b0f3ef7256d805ea5ccf
fangqk1991 <me@fangqk.com>
2018-05-07 16:51:22 +0800

    MyResque

---
+++ library/MyResque.php
@@ -0,0 +1,50 @@
+<?php
+
+include_once __DIR__ . '/../vendor/chrisboulton/php-resque/lib/Resque.php';
+
+class MyResque
+{
+    private static $_instance = NULL;
+
+    private $host;
+    private $port;
+
+    private function __construct()
+    {
+    }
+
+    private static function getInstance()
+    {
+        if(!(self::$_instance instanceof self))
+        {
+            self::$_instance = new self();
+        }
+
+        return self::$_instance;
+    }
+
+    private function __clone()
+    {
+        die('Clone is not allowed.' . E_USER_ERROR);
+    }
+
+    public static function init($host, $port)
+    {
+        $obj = self::getInstance();
+        $obj->host = $host;
+        $obj->port = $port;
+
+        Resque::setBackend(sprintf('%s:%s', $obj->host, $obj->port));
+    }
+
+	public static function enqueue($queue, $task, $args)
+	{
+		try
+		{
+			Resque::enqueue($queue, $task, $args, true);
+		}
+		catch(Exception $e)
+		{
+		}
+	}
+}
