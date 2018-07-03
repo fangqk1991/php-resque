ca29ae40f21f6cefe7aa5ef0d5ab7635d80b130d
fangqk1991 <me@fangqk.com>
2018-07-03 16:45:07 +0800

    ScheduleJob

---
+++ library/Schedule/ScheduleJob.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace FC\Resque\Schedule;
+
+use Exception;
+use FC\Resque\Core\Resque;
+use InvalidArgumentException;
+
+class ScheduleJob
+{
+    private $_payload;
+
+    private static function redis()
+    {
+        return Resque::redis();
+    }
+
+    public function __construct($payload)
+    {
+        $this->_payload = $payload;
+
+        if (!isset($this->_payload['args'])) {
+            $this->_payload['args'] = array();
+        }
+
+        if (!isset($this->_payload['id'])) {
+            $this->_payload['id'] = md5(uniqid('', TRUE));
+        }
+
+//        $this->_payload = array(
+//            'class' => $class,
+//            'args'  => $args,
+//            'id'    => md5(uniqid('', TRUE)),
+//            'queue_time' => microtime(true),
+//        );
+    }
+
+    public function getArguments()
+    {
+        return $this->_payload['args'];
+    }
+
+    public function getJobID()
+    {
+        return $this->_payload['id'];
+    }
+
+    public function getClassName()
+    {
+        return $this->_payload['class'];
+    }
+
+    public function getQueue()
+    {
+        return $this->_payload['queue'];
+    }
+
+    private function redisKey_jobFlag()
+    {
+        return sprintf('schedule:queue:%s:job:%s', $this->getQueue(), $this->getJobID());
+    }
+
+    private function redisKey_jobPayload()
+    {
+        return sprintf('schedule:queue:%s:job:%s:payload', $this->getQueue(), $this->getJobID());
+    }
+
+    private function redisKey_jobsSetForQueue()
+    {
+        return sprintf('schedule:queue:%s:jobs-zset', $this->getQueue());
+    }
+
+    private function redisKey_jobsSet()
+    {
+        return sprintf('schedule:jobs-zset');
+    }
+
+    public function performAfterDelay($seconds)
+    {
+        $this->performAtTime(time() + $seconds);
+    }
+
+    public function performAtTime($timestamp)
+    {
+        $timestamp = intval($timestamp);
+        $seconds = $timestamp - time();
+
+        if($seconds <= 0)
+        {
+            return ;
+        }
+
+        $redis = self::redis();
+
+        $flagKey = $this->redisKey_jobFlag();
+        $jobKey = $this->redisKey_jobPayload();
+
+        $redis->set($flagKey, 1);
+        $redis->expire($flagKey, $seconds);
+
+        $redis->set($jobKey, json_encode($this->_payload));
+        $redis->zAdd($this->redisKey_jobsSetForQueue(), $timestamp, $jobKey);
+        $redis->zAdd($this->redisKey_jobsSet(), $timestamp, $jobKey);
+    }
+
+    public function cancel()
+    {
+        $redis = self::redis();
+
+        $redis->del($this->redisKey_jobFlag());
+        $redis->del($this->redisKey_jobPayload());
+        $redis->del($this->redisKey_jobsSetForQueue());
+        $redis->del($this->redisKey_jobsSet());
+    }
+
+    public function run()
+    {
+        Resque::enqueue($this->getQueue(), $this->getClassName(), $this->getArguments());
+
+        $redis = self::redis();
+        $redis->del($this->redisKey_jobFlag());
+        $redis->del($this->redisKey_jobPayload());
+        $redis->del($this->redisKey_jobsSetForQueue());
+        $redis->del($this->redisKey_jobsSet());
+    }
+
+    public static function find($queue, $uid)
+    {
+        $obj = new self(array(
+            'id' => $uid,
+            'queue' => $queue
+        ));
+
+        return self::jobWithPayloadKey($obj->redisKey_jobPayload());
+    }
+
+    public static function jobWithPayloadKey($jobKey)
+    {
+        $redis = self::redis();
+
+        $payload = json_decode($redis->get($jobKey), TRUE);
+        if(is_array($payload))
+        {
+            return new self($payload);
+        }
+
+        return NULL;
+    }
+
+    public static function create($queue, $uid, $class, array $args)
+    {
+        $obj = self::find($queue, $uid);
+
+        if($obj instanceof self)
+        {
+            throw new Exception(
+                sprintf('ScheduleJob: %s:%s exists.', $queue, $uid)
+            );
+        }
+
+        if(!is_array($args)) {
+            throw new InvalidArgumentException(
+                'Supplied $args must be an array.'
+            );
+        }
+
+        $payload = array(
+            'queue' => $queue,
+            'class' => $class,
+            'args'  => $args,
+            'id'    => empty($uid) ? md5(uniqid('', TRUE)) : $uid,
+            'queue_time' => microtime(true),
+        );
+
+        return new ScheduleJob($payload);
+    }
+}
